b8d4160c54677a8e2bf7243fcf776508f0ee7916
Gargameth <stragorhelm@gmail.com>
2023-01-25 14:00:29 +0100

    Add utils file; separates selenium from junit.

--- AutomatedTests/src/test/java/Tests/TestBase.java
+++ AutomatedTests/src/test/java/Tests/TestBase.java
@@ -1,17 +1,14 @@
 package Tests;
 
+import Utils.Utils;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
 
     //static public LoginPage loginPage;
     //static public ProfilePage profilePage;
-    String driverPath = "C:\\Users\\Gargameth\\Desktop\\chromedriver.exe";
-    public static WebDriver driver;
 
     private final Dotenv dotenv = Dotenv.configure()
             .filename("URLs.env")
@@ -23,15 +20,12 @@ public class TestBase {
 
     @Test
     public void CheckIfBuildWorkingTest(){
-        System.setProperty("webdriver.chrome.driver", driverPath);
-        driver = new ChromeDriver();
-        driver.get(location);
-        System.out.println("Page reached, and closed.");
+        Utils.driver = Utils.GetDriver();
+        Utils.driver.get(location);
     }
 
     @AfterAll
     public static void CleanupDriver(){
-        driver.quit();
+        Utils.DriverQuit();
     }
-
 }
---
+++ AutomatedTests/src/test/java/Utils/Utils.java
@@ -0,0 +1,26 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Utils {
+
+    public static WebDriver driver;
+
+    public Utils(){
+    }
+
+    public static WebDriver GetDriver(){
+        if (driver == null) {
+            String path = "C:\\Users\\Gargameth\\Desktop\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", path);
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void DriverQuit(){
+        driver.quit();
+        driver = null;
+    }
+}
